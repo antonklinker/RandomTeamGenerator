6a33eb5e160e42c6fdf5e8febcb2fa41c2f4125a
Anton Klinker <klinker@live.dk>
2020-11-18 02:53:55 +0100

    added the possibility to drag the window around

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,24 +1,16 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Color;
-import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class Controller {
     public TextField teamsamount;
@@ -29,12 +21,17 @@ public class Controller {
     public AnchorPane mybackground;
     public AnchorPane team1background;
     public AnchorPane team2background;
-    public AnchorPane potbackground;
+    //public AnchorPane potbackground;
     public Button resetbutton;
     public Button addToEveryone;
     public Slider teamsamountSlider;
     public Label numberofteams;
     public AnchorPane teamsbackground;
+    public Canvas background;
+    public GraphicsContext gc;
+    public Button exitButton;
+    public AnchorPane dragBar;
+    public double currentX, currentY;
 
 
     //AnchorPane[] teamBackgrounds = new AnchorPane[4];
@@ -43,38 +40,64 @@ public class Controller {
     Label[] playerDisplays;
     TextField[] amountOfPlayers;
     ArrayList<String> everyone = new ArrayList<>();
+    ArrayList<String> addedPeople;
     //List[] teams = new List[100];
     List[] teams;
     int amountOfPeople;
 
     public void initialize() {
+        gc = background.getGraphicsContext2D();
         playersamounttext.setText("2");
         mybackground.getChildren().remove(team1background);
         mybackground.getChildren().remove(team2background);
+        gc.setFill(Color.DARKBLUE);
+        gc.setGlobalAlpha(0.3);
+        for (int i=0; i<(int)background.getHeight(); i++) {
+            gc.fillRect(0, i+(i*10), background.getWidth(), 1);
+            gc.fillRect(i+(i*10), 0, 1, background.getHeight());
+        }
+
     }
 
-    public void generateTeams(ActionEvent actionEvent) throws Exception {
+    /*public void generateTeams(ActionEvent actionEvent) throws Exception {
         if (everyone.size()>1) {
             realGenerate();
             everyone.clear();
         } else {
             System.out.println("You need to have at least 2 people to generate teams");
         }
-    }
+    }*/
 
     public void realGenerate() {
         try {
+            teamsbackground.getChildren().clear();
+            addToEveryone.setText("NEW TEAMS");
             int amountOfTeams = (int)teamsamountSlider.getValue();
-            teamBackgrounds = new AnchorPane[amountOfTeams];
-            playerDisplays = new Label[amountOfPeople];
-            teams = new List[amountOfTeams];
+
+
+            while ((double)everyone.size()%amountOfTeams!=0) {
+                everyone.add("--");
+                amountOfPeople++;
+            }
             if (everyone.size()%amountOfTeams==0) {
+                teamBackgrounds = new AnchorPane[amountOfTeams];
+                playerDisplays = new Label[amountOfPeople];
+                teams = new List[amountOfTeams];
                 for (int i = 0; i < amountOfTeams; i++) {
                     teamBackgrounds[i] = new AnchorPane();
-                    teamBackgrounds[i].setLayoutX(0 + (100 * i));
-                    teamBackgrounds[i].setLayoutY(0);
                     teamBackgrounds[i].setPrefSize(92, 246);
-                    teamBackgrounds[i].setStyle("-fx-border-color:BLUE; -fx-background-color: #090364");
+                    if (i%2==0) {
+                        teamBackgrounds[i].setStyle("-fx-border-color:BLUE; -fx-background-color: #090364");
+                    } else {
+                        teamBackgrounds[i].setStyle("-fx-border-color:BLUE; -fx-background-color: #090541");
+                    }
+                    if (i<5) {
+                        teamBackgrounds[i].setLayoutX(0 + (100 * i));
+                        teamBackgrounds[i].setLayoutY(0);
+                    } else {
+                        teamBackgrounds[i].setLayoutX(0 + (100 * (i-5)));
+                        teamBackgrounds[i].setLayoutY(252);
+                    }
                     teamsbackground.getChildren().add(teamBackgrounds[i]);
                     teams[i] = new ArrayList<String>();
                 }
@@ -82,40 +105,48 @@ public class Controller {
 
                 for (int i = 0; i < amountOfTeams; i++) {
                     Label header = new Label();
+                    Label underscore = new Label();
                     header.setText("Team " + (i + 1));
-                    header.setLayoutY(15);
-                    header.setLayoutX(15);
-                    header.setTextFill(Color.BLUE);
-                    teamBackgrounds[i].getChildren().add(header);
+                    header.setLayoutY(6);
+                    header.setLayoutX(27);
+                    if (i==9) {
+                        header.setLayoutX(26);
+                    }
+                    underscore.setText("__________________");
+                    underscore.setLayoutY(12);
+                    underscore.setLayoutX(2);
+                    underscore.setStyle("-fx-text-fill: #1696ff;");
+                    header.setStyle("-fx-text-fill: #1696ff;");
+                    teamBackgrounds[i].getChildren().addAll(header, underscore);
                     for (int j = 0; j < amountOfPeople / amountOfTeams; j++) {
                         int randNumber = (int) (Math.random() * everyone.size());
                         teams[i].add(everyone.get(randNumber));
+                        //addedPeople.add("everyone.get(0)");
                         everyone.remove(randNumber);
-                        System.out.println("Team " + i + " has " + teams[i].get(j));
                         playerDisplays[j] = new Label();
-                        playerDisplays[j].setLayoutX(17);
+                        playerDisplays[j].setLayoutX(10);
                         playerDisplays[j].setLayoutY(30 + (20 * j));
-                        playerDisplays[j].setTextFill(Color.BLUE);
+                        //playerDisplays[j].setTextFill(Color.BLUE);
+                        playerDisplays[j].setStyle("-fx-text-fill: #1696ff");
                         playerDisplays[j].setText((String) teams[i].get(j));
                         teamBackgrounds[i].getChildren().add(playerDisplays[j]);
                     }
                 }
+                everyone.clear();
             }
             else {
-                System.out.println("The teams need to be equal");
             }
         } catch (NumberFormatException e){
-            System.out.println("You need to input a number");
+            e.printStackTrace();
         }
     }
 
     public void addPeople(ActionEvent actionEvent) {
         try {
-            potbackground.getChildren().clear();
+            //potbackground.getChildren().clear();
             for (int i = 0; i < amountOfPlayers.length; i++) {
                 try {
 
-                    //System.out.println(amountOfPlayers[i].getText());
                     if (!amountOfPlayers[i].getText().equals("")) {
                         //System.out.println(i + " was empty and has been removed");
                         //everyone.remove(i);
@@ -126,12 +157,17 @@ public class Controller {
                 }
             }
             amountOfPeople = everyone.size();
-            for (int i = 0; i < everyone.size(); i++) {
+            /*for (int i = 0; i < everyone.size(); i++) {
                 Label potman = new Label();
                 potman.setLayoutX(56);
                 potman.setLayoutY(30 + (15*i));
                 potman.setText(everyone.get(i));
                 potbackground.getChildren().add(potman);
+            }*/
+            if (everyone.size()>1) {
+                realGenerate();
+            } else {
+                System.out.println("You need to have at least 2 people to generate teams");
             }
         }
         catch (NullPointerException e) {
@@ -144,23 +180,13 @@ public class Controller {
 
     public void setAmountText(MouseEvent mouseEvent) {
         playersamounttext.setText(String.valueOf((int)playersamount.getValue()));
+        add_TextFields();
     }
 
-    public void resetprogram(ActionEvent actionEvent) {
-        try {
-            addToEveryone.setVisible(false);
-        for (int i=0; i<amountOfPlayers.length; i++) {
-            mybackground.getChildren().remove(amountOfPlayers[i]);
-        }
 
-        System.out.println("NOT WORKING");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println(e);
-        }
-    }
 
-    public void add_TextFields(MouseEvent mouseEvent) throws InterruptedException {
-        System.out.println("WORKING");
+
+    public void add_TextFields() {
         mybackground.getChildren().clear();
 
 
@@ -169,7 +195,7 @@ public class Controller {
 
         for (int i=0; i<(int)playersamount.getValue(); i++) {
             amountOfPlayers[i] = new TextField();
-            amountOfPlayers[i].setStyle("-fx-border-color: BLUE; -fx-control-inner-background: #090364");
+            amountOfPlayers[i].setStyle("-fx-border-color: BLUE; -fx-control-inner-background: #090364; -fx-text-fill: #1696ff");
             amountOfPlayers[i].setLayoutX(14);
             amountOfPlayers[i].setLayoutY(20 + (30*i));
             mybackground.getChildren().add(amountOfPlayers[i]);
@@ -179,21 +205,39 @@ public class Controller {
 
 
             addToEveryone = new Button();
-            addToEveryone.setStyle("-fx-border-color: BLUE; -fx-background-color: #020139");
-            addToEveryone.setTextFill(Color.BLUE);
+            addToEveryone.setStyle("-fx-border-color: BLUE; -fx-background-color: #020139; -fx-text-fill: #1696ff");
+            //addToEveryone.setTextFill(Color.BLUE);
             addToEveryone.setLayoutX(14);
             addToEveryone.setLayoutY(20 + (30 * amountOfPlayers.length));
             addToEveryone.setPrefWidth(150);
-            addToEveryone.setText("Add the people");
+            addToEveryone.setText("GENERATE");
             addToEveryone.setOnAction(this::addPeople);
             mybackground.getChildren().add(addToEveryone);
         }
         catch (Exception e ){
-            System.out.println("Something went wrong");
+            e.printStackTrace();
         }
     }
 
     public void setTeamsAmount(MouseEvent mouseEvent) {
         numberofteams.setText(String.valueOf((int)teamsamountSlider.getValue()));
+    }
+
+    public void closeApp(ActionEvent actionEvent) {
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+    public void moveApp(MouseEvent mouseEvent) {
+        Stage stage = (Stage) dragBar.getScene().getWindow();
+        stage.setX(mouseEvent.getScreenX()-currentX);
+        stage.setY(mouseEvent.getScreenY()-currentY);
+    }
+
+    public void recordMousePosition(MouseEvent mouseEvent) {
+        currentX=mouseEvent.getX();
+        currentY=mouseEvent.getY();
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,36 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+    private int height = 800;
+    private int width = 800;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.close();
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1600, 800));
+        //Image image = new Image("sample/icons/logo.jpg");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setStyle("-fx-background-color: BLUE");
+
+        ToolBar toolBar = new ToolBar();
+        toolBar.setPrefHeight(height);
+        toolBar.setMinHeight(height);
+        toolBar.setMaxHeight(height);
+        //toolBar.getItems().add(new WindowButtons());
+
+        borderPane.setTop(toolBar);
+        //primaryStage.getIcons().add(image);
+        primaryStage.setTitle("Team Generator");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, width, height));
         primaryStage.show();
     }
 
